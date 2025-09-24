e509308cda0c5be00f54c672274b27ba50af137c
Sid Jr <sidalbertok@gmail.com>
2025-09-24 15:49:42 -0300

    Update Kalleotopia.sql

--- Banco dados/Kalleotopia.sql
+++ Banco dados/Kalleotopia.sql
@@ -3,7 +3,7 @@ CREATE DATABASE kalleotopia;
 USE kalleotopia;
 
 CREATE TABLE personagem (
-   Id_personagem INT PRIMARY KEY AUTO_INCREMENT,
+   id_personagem INT PRIMARY KEY AUTO_INCREMENT,
    nome_personagem VARCHAR(55) NOT NULL,
    classe VARCHAR(55) NOT NULL,
    dano INT NOT NULL,
@@ -28,7 +28,17 @@ CREATE TABLE classe (
 
 CREATE TABLE mobs (
   id_mob INT PRIMARY KEY AUTO_INCREMENT,
-  nome_mob VARCHAR(55) NOT NULL
+  nome_mob VARCHAR(55) NOT NULL,
+  dano_mob int not null,
+  mobilidade int not null
+);
+
+CREATE TABLE evolucao_classe(
+  id_evolucao int primary key auto_increment,
+  nome_evolucao varchar(100) not null,
+  info_evolucao varchar(100)not null,
+  id_personagem INT,
+FOREIGN KEY (id_personagem) REFERENCES personagem(Id_personagem)
 );
 
 CREATE TABLE personagem_classe (
@@ -37,4 +47,11 @@ CREATE TABLE personagem_classe (
   PRIMARY KEY (id_personagem, id_classe),
   FOREIGN KEY (id_personagem) REFERENCES personagem(Id_personagem),
   FOREIGN KEY (id_classe) REFERENCES classe(id_classe)
+);
+
+CREATE TABLE usuario_personagem(
+id_personagem int not null,
+id_usuario int not null,
+FOREIGN KEY (id_personagem) REFERENCES personagem(Id_personagem),
+FOREIGN KEY( id_usuario) REFERENCES usuario(id_usuario)
 );
