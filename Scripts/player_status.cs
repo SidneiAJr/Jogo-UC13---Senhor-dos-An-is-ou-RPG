c69e320569f339d68b838f266cacefddbbd58c90
Sid Jr <sidalbertok@gmail.com>
2025-09-24 15:53:42 -0300

    Create player_status.cs

---
+++ Scripts/player_status.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus: MonoBehaviour
+{
+   public int Vidamaxima;
+   public int Vida;
+   public double Dano_Player;
+   public double armadura;
+   public 
+}
