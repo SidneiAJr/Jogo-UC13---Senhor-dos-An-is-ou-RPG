3300bcdc666ff54c837d664bf3066bd8d46b498b
Sid Jr <sidalbertok@gmail.com>
2025-09-25 11:31:22 -0300

    Update Classe.cs

--- Scripts/Classe.cs
+++ Scripts/Classe.cs
@@ -1,1 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class PlayerStatus : MonoBehaviour
+{
+    public int VidaMaxima;
+    public int Vida;
+    public double Dano_Player;
+    public int Armadura_Player;
+    public double Velocidade_Ataque;
+    public double RouboVida;
+    public int Level;
+
+    void Start() {
+        Vida = VidaMaxima; // inicia com vida cheia
+    }
+}
